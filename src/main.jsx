482bba669487a0d775a691faa3588e0439c9e545
vymat <vymatvienko@gmail.com>
2024-03-25 00:43:40 +0300

    Fourth commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,6 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>,
-//   {/* </React.StrictMode>, */}
-// )
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <Provider store={store}>
